fe517f47efc07c4724c63c93c18e2677ae31b4d8
Emile Esmaili <emile.esmaili@gmail.com>
2022-07-21 16:58:11 +0200

    udapted redirect uri

--- streamlit_assets/components.py
+++ streamlit_assets/components.py
@@ -18,7 +18,8 @@ class User:
         self.secret_id = '7274e34729fc407d9f0bef405fd24805'
         self.redirect_uri = 'http://127.0.0.1:8080'
         self.scope = "user-library-read, playlist-read-private, user-read-private, user-read-playback-state, user-top-read, user-follow-read, user-read-currently-playing, user-read-recently-played"
-        self.auth_manager = SpotifyOAuth(client_id=self.client_id, client_secret=self.secret_id, redirect_uri=self.redirect_uri, scope=self.scope)
+        self.auth_manager = SpotifyOAuth(client_id=self.client_id, client_secret=self.secret_id, redirect_uri=self.redirect_uri, scope=self.scope, show_dialogue=True)
+        
         self.sp = spotipy.Spotify(auth_manager=self.auth_manager)
     
     @staticmethod
